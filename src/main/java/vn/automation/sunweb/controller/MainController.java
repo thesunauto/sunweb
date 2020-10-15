f56575164e5bf427b5d91d7c44953f59b5ea3702
thesunauto <thesunautomt@gmail.com>
2020-10-15 16:17:03 +0700

    15.10

--- src/main/java/vn/automation/sunweb/controller/MainController.java
+++ src/main/java/vn/automation/sunweb/controller/MainController.java
@@ -1,12 +1,24 @@
 package vn.automation.sunweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index(){
+    public String index(Model model) {
+        model.addAttribute("index","hello");
         return "client/index";
+    }
+
+    @GetMapping("/listPost")
+    public String listPost() {
+        return "client/listPost";
+    }
+
+    @GetMapping("/singlePost")
+    public String singlePost() {
+        return "client/singlePost";
     }
 }
