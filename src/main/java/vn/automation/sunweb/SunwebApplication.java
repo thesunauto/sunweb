09108964f9d818716d380bd3675775c3c6c6bfde
thesunautomt <thesunautomt@gmail.com>
2020-10-21 11:35:56 +0700

    uploadFile

--- src/main/java/vn/automation/sunweb/SunwebApplication.java
+++ src/main/java/vn/automation/sunweb/SunwebApplication.java
@@ -2,8 +2,11 @@ package vn.automation.sunweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import vn.automation.sunweb.storage.StorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SunwebApplication {
 
     public static void main(String[] args) {
