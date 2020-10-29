fbdc1c44081c474ea83434a61342b90ea61913d0
thesunautomt <thesunautomt@gmail.com>
2020-10-29 11:16:36 +0700

    xong them Nguoi dung

--- src/main/java/vn/automation/sunweb/controller/AdminRestController.java
+++ src/main/java/vn/automation/sunweb/controller/AdminRestController.java
@@ -13,21 +13,25 @@ import vn.automation.sunweb.service.UserService;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
 @RequestMapping("/api")
 @RestController
 public class AdminRestController {
 
-    @Autowired private UserService userService;
+    @Autowired
+    private UserService userService;
 
     @PostMapping("/getlistuser")
-    public List<UserResponse> getListUser(){
+    public List<UserResponse> getListUser() {
         List<UserResponse> li = new ArrayList<>();
-        userService.select().forEach(value -> {li.add(new UserResponse(value.getUsername(),value.getPassword(),value.getName(),value.getDatecreated().getDayOfMonth()+"/"+value.getDatecreated().getMonthValue()+"/"+value.getDatecreated().getYear(),value.getPhone(),value.getEmail(),value.getRole(),value.getIsdelete()));});
+        userService.select().forEach(value -> {
+            li.add(new UserResponse(value.getUsername(), value.getPassword(), value.getName(), value.getDatecreated().getDayOfMonth() + "/" + value.getDatecreated().getMonthValue() + "/" + value.getDatecreated().getYear(), value.getPhone(), value.getEmail(), value.getRole(), value.getIsdelete()));
+        });
         return li;
     }
 
     @PostMapping("/save")
-    public ResponseEntity  addUser(@RequestBody UserResponse userResponse){
+    public ResponseEntity addUser(@RequestBody UserResponse userResponse) {
         User user = new User();
         user.setName(userResponse.getName());
         user.setUsername(userResponse.getUsername());
@@ -37,11 +41,7 @@ public class AdminRestController {
         user.setRole(userResponse.getRole());
         user.setDatecreated(LocalDateTime.now());
         user.setIsdelete(0);
-        if(userService.save(user)){
-        return ResponseEntity.ok().body("toastr.success('Thêm bài viết thành công', '', {positionClass: 'md-toast-top-right'});$('#toast-container').attr('class','md-toast-top-right');");
-    }else{
-            return ResponseEntity.ok().body("toastr.danger('Thêm bài viết thất bại', '', {positionClass: 'md-toast-top-right'});$('#toast-container').attr('class','md-toast-top-right');");
-
-        }
+        userService.save(user);
+        return ResponseEntity.ok().body(user);
     }
 }
